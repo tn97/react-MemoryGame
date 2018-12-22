1c903f52c872918a8553595fc85d93dd11d8b634
Timothy Na <runy385@gmail.com>
2018-12-22 17:29:09 -0500

    Updates

---
+++ precache-manifest.daeeef1cfaae66b1ebeb8f1881dc1447.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02c43fbfdc09285a12d3",
+    "url": "/react-memorygame/static/js/runtime~main.02c43fbf.js"
+  },
+  {
+    "revision": "39152bc6bf087416c605",
+    "url": "/react-memorygame/static/js/main.39152bc6.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/react-memorygame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "39152bc6bf087416c605",
+    "url": "/react-memorygame/static/css/main.2530fe73.chunk.css"
+  },
+  {
+    "revision": "abf106a0e866e0443998e3e090494aa2",
+    "url": "/react-memorygame/index.html"
+  }
+];
